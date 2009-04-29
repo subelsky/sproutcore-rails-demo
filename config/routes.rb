5f91af21511df22bec06f9a9d506dcc8fc89dcee
Mike Subelsky <github@mikeshop.net>
2009-04-29 17:20:57 -0400

    Saving my work

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :recipes
+
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
